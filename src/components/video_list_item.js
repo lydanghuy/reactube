e3e6b152d6f4eb873e09405acba7ba516c9e064a
lydanghuy <lydanghuy151196@gmail.com>
2018-05-14 02:19:18 +0700

    search bar

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,25 +1,25 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-const VideoListItem = ({video}) => {
-  // const video = props.video; console.log(video);
+const VideoListItem = ({ video, onVideoSelect }) => {
+	// const video = props.video; console.log(video);
 
-  const imageUrl = video.snippet.thumbnails.default.url;
+	const imageUrl = video.snippet.thumbnails.default.url;
 
-  return (
-    <li className="list-group-item">
-      <div className="video-list media">
-        <div className="media-left">
-          <img className="media-object" src={imageUrl}/>
-        </div>
+	return (
+		<li onClick={() => onVideoSelect(video)} className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
 
-        <div className="media-body">
-          <div className="media-heading">{video.snippet.title}</div>
-        </div>
+				<div className="media-body">
+					<div className="media-heading">{video.snippet.title}</div>
+				</div>
 
-      </div>
+			</div>
 
-    </li>
-  );
+		</li>
+	);
 };
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -4,27 +4,43 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
+import _ from 'lodash';
+
 const API_KEY = "AIzaSyDptlRyslOW_I54Ur8rBSnZxRJrNrfgtMc";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { videos: [] };
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('surfboard');
+	}
 
-    YTSearch({ key: API_KEY, term: "surfboards" }, data => {
-      this.setState({ videos: data });
-    });
-  }
+	videoSearch(query) {
+		YTSearch({ key: API_KEY, term: query }, data => {
+			this.setState({
+				videos: data,
+				selectedVideo: data[0]
+			});
+		});
+	}
 
-  render() {
-    return (
-      <div>
-        <SearchBar />
-		<VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
-      </div>
-    );
-  }
+	render() {
+		const videoSearch = _.debounce( (term) => { this.videoSearch(term) }, 300);
+
+		return (
+			<div>
+				{/* <SearchBar onSearchTermChange={term => this.videoSearch(term)} /> */}
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+					videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
